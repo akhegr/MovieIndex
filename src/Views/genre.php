fba87ec0c11b7344789a312467659956a48c3a52
Aksel Gregersen <akhegr@gmail.com>
2025-03-02 18:50:15 +0100

    Reuse setup on all pages

--- src/Views/genre.php
+++ src/Views/genre.php
@@ -1,25 +1,23 @@
-<h1>Genre overview - <?= $genre->name ?></h1>
-<p><b>Total amount of movies:</b> <?= $genre->movies->totalResults ?> movies</p>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col" width="20%">Cover</th>
-      <th scope="col" width="70%">Title</th>
-      <th scope="col" width="10%"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-        foreach ($genre->movies->results as $movie) : ?>
-            <tr>
-                <td><img src="https://image.tmdb.org/t/p/w92/<?= $movie->poster_path ?>" width/></td>
-                <td><?= $movie->original_title ?></td>
-                <td><a href="/description?movieId=<?= $movie->id ?>" class="btn btn-secondary">Description</a></td>
-            </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-
-
-
-    
+<div>
+  <h1 class="text-center isplay-4">Genre overview - <?= $genre->name ?></h1>
+  <p><b>Total amount of movies:</b> <?= $genre->movies->totalResults ?> movies</p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" width="20%">Cover</th>
+        <th scope="col" width="70%">Title</th>
+        <th scope="col" width="10%"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+          foreach ($genre->movies->results as $movie) : ?>
+              <tr>
+                  <td><img src="https://image.tmdb.org/t/p/w92/<?= $movie->poster_path ?>" width/></td>
+                  <td><?= $movie->original_title ?></td>
+                  <td><a href="/description?movieId=<?= $movie->id ?>" class="btn btn-secondary">Description</a></td>
+              </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</div>   
--- src/Views/index.php
+++ src/Views/index.php
@@ -1,32 +1,33 @@
-<h1>Genre overview</h1>
-<div class="container">
-    <div class="row">
-        <?php foreach($genres as $genre) : ?>
-            <div class="col-4">
-                <p><h3><?= $genre->name ?></h3>
-                <br>Click <a href="/genre?genreId=<?= $genre->id ?>">here</a> to enter genre
-                <br><b>Total amount of movies:</b> <?= $genre->movies->totalResults ?> movies</p>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col" width="20%">Cover</th>
-                        <th scope="col" width="70%">Title</th>
-                        <th scope="col" width="10%"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            $limitedMovieList = array_slice($genre->movies->results, 0, 5);
-                            foreach ($limitedMovieList as $movie) : ?>
-                                <tr>
-                                    <td><img src="https://image.tmdb.org/t/p/w92/<?= $movie->poster_path ?>" width/></td>
-                                    <td><?= $movie->original_title ?></td>
-                                    <td><a href="/description?movieId=<?= $movie->id ?>" class="btn btn-light">Description</a></td>
-                                </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        <?php endforeach; ?>
+<div>
+    <h1 class="text-center isplay-4">Genre overview</h1>
+    <div class="container">
+        <div class="row">
+            <?php foreach($genres as $genre) : ?>
+                <div class="col-4">
+                    <p><h3><?= $genre->name ?>&nbsp;&nbsp;<a href="/genre?genreId=<?= $genre->id ?>" class="btn btn-info">Enter the genre</a></h3>
+                    <br><b>Total amount of movies:</b> <?= $genre->movies->totalResults ?> movies</p>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col" width="20%">Cover</th>
+                            <th scope="col" width="70%">Title</th>
+                            <th scope="col" width="10%"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $limitedMovieList = array_slice($genre->movies->results, 0, 5);
+                                foreach ($limitedMovieList as $movie) : ?>
+                                    <tr>
+                                        <td><img src="https://image.tmdb.org/t/p/w92/<?= $movie->poster_path ?>" width/></td>
+                                        <td><?= $movie->original_title ?></td>
+                                        <td><a href="/description?movieId=<?= $movie->id ?>" class="btn btn-secondary">Description</a></td>
+                                    </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 </div>
