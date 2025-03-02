522e8cae53156afb7224b256ec5ff2d251742e19
Aksel Gregersen <akhegr@gmail.com>
2025-03-02 17:15:53 +0100

    Implement genre overview

---
+++ src/Views/genre.php
@@ -0,0 +1,25 @@
+<h1>Genre overview - <?= $genre->name ?></h1>
+<p><b>Total amount of movies:</b> <?= $genre->movies->totalResults ?> movies</p>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col" width="20%">Cover</th>
+      <th scope="col" width="70%">Title</th>
+      <th scope="col" width="10%"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+        foreach ($genre->movies->results as $movie) : ?>
+            <tr>
+                <td><img src="https://image.tmdb.org/t/p/w92/<?= $movie->poster_path ?>" width/></td>
+                <td><?= $movie->original_title ?></td>
+                <td><a href="/description?movieId=<?= $movie->id ?>">Enter</a></td>
+            </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+
+
+    
