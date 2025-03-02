e935252d986cebb4597a6c5b5425104bb89f4d3b
Aksel Gregersen <akhegr@gmail.com>
2025-03-02 17:59:13 +0100

    Cleanup routes

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -9,29 +8,26 @@
         <link rel="stylesheet" href="/css/site.css" />
     </head>
     <body>
-        <header b-bddmc6yalx>
-            <nav b-bddmc6yalx class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-                <div b-bddmc6yalx class="container-fluid">
+        <header>
+            <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+                <div class="container-fluid">
                     <a class="navbar-brand" href="/">MovieIndex</a>
-                    <button b-bddmc6yalx class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
-                        <span b-bddmc6yalx class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div b-bddmc6yalx class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                        <ul b-bddmc6yalx class="navbar-nav flex-grow-1">
-                            <li b-bddmc6yalx class="nav-item">
+                    <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                        <ul class="navbar-nav flex-grow-1">
+                            <li b-class="nav-item">
                                 <a class="nav-link text-dark" href="/">Home</a>
-                            </li>
-                            <li b-bddmc6yalx class="nav-item">
-                                <a class="nav-link text-dark" href="/description?movieId=550">Description</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         </header>
-        <div b-bddmc6yalx class="container">
-            <main b-bddmc6yalx role="main" class="pb-3">
+        <div class="container">
+            <main role="main" class="pb-3">
                 <?php
 
                 use App\Controllers\HomeController;
@@ -58,6 +54,6 @@
         </footer>
         <script src="/lib/jquery/dist/jquery.min.js"></script>
         <script src="/lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="/js/site.js?v=hRQyftXiu1lLX2P9Ly9xa4gHJgLeR1uGN5qegUobtGo"></script>
+        <script src="/js/site.js"></script>
     </body>
 </html>
